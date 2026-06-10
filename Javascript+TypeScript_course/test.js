ceac1e09680a2b24403a9ac2a603f3da73279173
Luiz Messias <luizgdsmdev@gmail.com>
2026-06-10 07:41:32 -0300

    update

---
+++ Javascript+TypeScript_course/test.js
@@ -0,0 +1,8 @@
+//equivalent by using a new variable
+let myName = '"Luiz"';
+let newName = myName + " Gustavo";
+console.log(newName); //shows 'Luiz Gustavo'
+console.log(myName); //shows 'Luiz'
+myName = "Luizzzzz";
+console.log(newName); //shows 'Luiz Gustavo'
+console.log(myName); //shows 'Luiz'
